669ac55ad94b172643337ff0c55dd8e6d94d21c4
YSW2 <jrgyber@gmail.com>
2024-11-11 16:40:56 +0900

    feat: 메인 애플리케이션 클래스 구현
    - 전체 구매 프로세스 흐름 제어 로직 구현
    - 상품 서비스와 할인 서비스 통합
    - 사용자 입력과 출력 흐름 관리
    - 에러 처리 및 재시도 로직 추가
    - 추가 구매 프로세스 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,114 @@
+import { ProductService } from './service/ProductService.js';
+import { DiscountService } from './service/DiscountService.js';
+import { InputView } from './view/InputView.js';
+import { OutputView } from './view/OutputView.js';
+
 class App {
-  async run() {}
+  constructor() {
+    this.productService = new ProductService();
+    this.discountService = new DiscountService();
+  }
+
+  async run() {
+    await this.initialize();
+    await this.startShoppingProcess();
+  }
+
+  async initialize() {
+    try {
+      await this.productService.initialize();
+      OutputView.printWelcome();
+    } catch (error) {
+      OutputView.printError(error.message);
+      throw error;
+    }
+  }
+
+  async startShoppingProcess() {
+    try {
+      do {
+        await this.processOneShopping();
+      } while (await this.checkAdditionalPurchase());
+    } catch (error) {
+      OutputView.printError(error.message);
+    }
+  }
+
+  async processOneShopping() {
+    OutputView.printProducts(this.productService.getProductList());
+    await this.processPurchase();
+    await this.processPromotions();
+    await this.processDiscount();
+    this.printReceipt();
+  }
+
+  async processPurchase() {
+    while (true) {
+      try {
+        const input = await InputView.readPurchaseItems();
+        this.productService.addToCart(input);
+        break;
+      } catch (error) {
+        OutputView.printError(error.message);
+      }
+    }
+  }
+
+  async processPromotions() {
+    const suggestions = this.productService.getAvailablePromotionSuggestions();
+
+    for (const suggestion of suggestions) {
+      OutputView.printPromotionSuggestion(
+        suggestion.productName,
+        suggestion.quantity
+      );
+
+      if (await InputView.readPromotionSuggestionResponse()) {
+        this.productService.addToCart(
+          `[${suggestion.productName}-${suggestion.quantity}]`
+        );
+      }
+    }
+
+    const shortages = this.productService.checkPromotionStockShortage();
+    for (const shortage of shortages) {
+      OutputView.printNormalPriceConfirmation(
+        shortage.productName,
+        shortage.normalQuantity
+      );
+
+      if (!(await InputView.readNormalPriceConfirmation())) {
+        // 프로모션 재고만큼만 구매하도록 카트 초기화 후 재시도
+        this.productService.clearCart();
+        return await this.processPurchase();
+      }
+    }
+
+    this.productService.applyPromotions();
+  }
+
+  async processDiscount() {
+    OutputView.printMembershipQuestion();
+    const useMembership = await InputView.readMembershipUse();
+
+    const cartSummary = this.productService.getCartSummary();
+    this.discountService.calculateDiscounts(
+      cartSummary.totalPrice,
+      cartSummary.promotionDiscount,
+      useMembership
+    );
+  }
+
+  printReceipt() {
+    const cartSummary = this.productService.getCartSummary();
+    const discountSummary = this.discountService.getDiscountSummary();
+    OutputView.printReceipt(cartSummary, discountSummary);
+  }
+
+  async checkAdditionalPurchase() {
+    OutputView.printAdditionalPurchaseQuestion();
+    return await InputView.readAdditionalPurchase();
+  }
 }
 
 export default App;
